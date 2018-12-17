44f4a66d188e092a020681fada839d110f8875f3
Ilya Kreymer <ikreymer@gmail.com>
2018-12-17 15:09:42 -0800

    improved setup: - remove base-root, base-browser and base-displayaudio independent - base-displayaudio: add gstreamer 1.4.14 source build + python bindings to displayaudio - base-browser: update fonts, add missing dejavu font, add other shared requirements - audio_stream.sh: simplify audio to just mp3 and opus streaming

--- vnc-ws-audio/audio_stream.sh
+++ vnc-ws-audio/audio_stream.sh
@@ -5,24 +5,12 @@ pulseaudio -D
 if [[ "$AUDIO_TYPE" == "opus" ]]; then
     echo "Starting OPUS WS Audio"
 
-    # opt 1
-    uwsgi --http-socket :6082 --gevent 4 --wsgi-file /app/audio_proxy.py &
-
-    # opt 2
-    #gst-launch-1.0 -v alsasrc ! audio/x-raw, channels=2, rate=24000 ! opusenc complexity=0 frame-size=2.5 ! webmmux streamable=true ! tcpserversink port=4720 &
-
-    #sleep 2
-
-    #websockify 6082 localhost:4720 &
-
-    # opt 3
-    #websockify 6082 -- gst-launch-1.0 -v alsasrc ! audio/x-raw, channels=2, rate=24000 ! opusenc complexity=0 frame-size=2.5 ! webmmux streamable=true ! tcpserversink port=6082 &
+    websockify 6082 -- gst-launch-1.0 -v pulsesrc buffer-time=128000 latency-time=32000 ! audioconvert ! opusenc frame-size=2.5 ! webmmux ! tcpserversink port=6082 &
 
 
 elif [[ "$AUDIO_TYPE" == "mp3" ]]; then
     echo "Starting MP3 WS Audio"
 
-    websockify 6082 -- gst-launch-1.0 -v alsasrc ! audio/x-raw, channels=2, rate=24000 ! lamemp3enc target=bitrate cbr=true bitrate=192 ! tcpserversink port=6082 &
-
+    websockify 6082 -- gst-launch-1.0 -v pulsesrc buffer-time=128000 latency-time=32000 ! audioconvert ! lamemp3enc target=bitrate cbr=true bitrate=192 ! tcpserversink port=6082 &
 fi
 
